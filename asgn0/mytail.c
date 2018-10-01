16311522768bc666cced78c928d394fe25b1c749
yuzu <ejcordov@ucsc.edu>
2018-09-30 19:23:22 -0700

    added error handling

--- asgn0/mytail.c
+++ asgn0/mytail.c
@@ -1,11 +1,15 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdio.h>
 #define MAX_CHAR_BUFFER 128
 #define TAIL_SIZE 10
 
 void tail_mode(int num_args,char **arg_names){
   for (int i=1 ; i<num_args ; i++){
     int fileopen = open(arg_names[i],O_RDONLY);
+    if (fileopen == -1){
+      perror ("error opening file %s"); 
+    }
     char tl[MAX_CHAR_BUFFER]={0};
     char tl_reversed[MAX_CHAR_BUFFER]={0};
     int current_number_lines = 0;
@@ -21,6 +25,9 @@ void tail_mode(int num_args,char **arg_names){
       tl_reversed[j] = tl[j];     
     }
     int fileclose = close(fileopen);
+    if (fileclose == -1){
+      perror ("error closing file %s"); 
+    }
     write (1,tl_reversed,sizeof(tl_reversed));
   }
 }
