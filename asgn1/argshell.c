8926f8b2b8bf87696f30a916da88a486912bf079
yuzu <ejcordov@ucsc.edu>
2018-10-22 03:51:26 -0700

    finished basic commands / commands w/ arguments

--- asgn1/argshell.c
+++ asgn1/argshell.c
@@ -3,26 +3,55 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <string.h>
+#include <stdlib.h>
+#define ARG_MAX 1024;
 
 extern char ** get_args();
+int status_exit = 0;
+char **arg_tree[1024];      
+int command_iterations = 0;      
+char *file_input [1024];
+char *file_output [1024];
+int *pipes [1024]; 
+
+
+void read_arguments(char **arguments){
+  if (arguments[0] == NULL) {
+      printf ("No arguments on line!\n");
+  } else if ( !strcmp (arguments[0], "exit")) {
+      printf ("Exiting...\n");
+      exit(status_exit);
+  }
+  arg_tree[0] = arguments;
+  for (int i = 0 ; arguments[i] != NULL; i++){
+    char cc = arguments[i][0];
+    if (cc == '>'){
+      arguments[i] = NULL;
+      file_output[i] = arguments[i+1];
+    }
+  }
+}
+
+void execute_arguments(){
+  int p_id = fork();
+  if (p_id==-1){
+    perror("pid = -1");
+  }
+  if (p_id == 0){
+    for (int i=0; i < command_iterations + 1 ; i++){
+      execvp (arg_tree[i][0],arg_tree[i]);
+    }
+  }
+}
 
 int
 main()
 {
-    int         i;
     char **     args;
-
     while (1) {
 	printf ("Command ('exit' to quit): ");
 	args = get_args();
-	for (i = 0; args[i] != NULL; i++) {
-	    printf ("Argument %d: %s\n", i, args[i]);
-	}
-	if (args[0] == NULL) {
-	    printf ("No arguments on line!\n");
-	} else if ( !strcmp (args[0], "exit")) {
-	    printf ("Exiting...\n");
-	    break;
-	}
+	read_arguments(args);
+	execute_arguments();
     }
 }
